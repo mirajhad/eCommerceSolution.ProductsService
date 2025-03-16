c7353bf8699501f5f246b2357c06b19847772ecb
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2025-03-16 12:30:38 +0530

    feat(BAL): Add Dependency Injection and Middleware
    - Created `DependencyInjection.cs` for Business and Data Access layers.
    - Added `ExceptionHandlingMiddleware.cs` for error handling in the pipeline.
    - Updated `ProductsMicroservice.API.csproj` to include `Newtonsoft.Json`.
    - Modified `Program.cs` to integrate new layers and middleware.

---
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace BusinessLogicLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataBusinessLogicLayer(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
--- ProductsMicroservice.API/Program.cs
+++ ProductsMicroservice.API/Program.cs
@@ -1,6 +1,29 @@
+using BusinessLogicLayer;
+using DataAccessLayer;
+using FluentValidation.AspNetCore;
+using ProductsMicroservice.API.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//ADD DAL AND BAL SERVICES
+builder.Services.AddDataAccessLayer();
+builder.Services.AddDataBusinessLogicLayer();
+
+builder.Services.AddControllers();
+
+//FLUENT VALIDATION
+builder.Services.AddFluentValidationAutoValidation();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+
+//Authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
