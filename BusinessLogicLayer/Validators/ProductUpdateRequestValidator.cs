4337455b7d70e145100efcc088cbee3792042e97
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2025-03-16 15:53:59 +0530

    feat(val): add product request validators
    - Introduced `ProductAddRequestValidator` for validating
      `ProductAddRequest` objects.
    - Introduced `ProductUpdateRequestValidator` for validating
      `ProductUpdateRequest` objects.
    - Implemented validation rules for required fields:
      `ProductName`, `Category`, `UnitPrice`, `QuantityInStock`,
      and `ProductID`.
    - Ensured category validation checks for valid enum values.

---
+++ BusinessLogicLayer/Validators/ProductUpdateRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogicLayer.DTO;
+using FluentValidation;
+
+
+namespace BusinessLogicLayer.Validators
+{
+    public class ProductUpdateRequestValidator : AbstractValidator<ProductUpdateRequest>
+    {
+        public ProductUpdateRequestValidator() 
+        {
+            RuleFor(x => x.ProductID).NotEmpty().WithMessage("Product ID is required");
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Product Name is required");
+            RuleFor(x => x.Category).IsInEnum().WithMessage("Category is required");
+            RuleFor(x => x.UnitPrice).NotEmpty().WithMessage("Unit Price is required");
+            RuleFor(x => x.QuantityInStock).NotEmpty().WithMessage("Quantity In Stock is required");
+        }
+    }
+}
