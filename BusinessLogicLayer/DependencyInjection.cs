6f3222cc8b24eca5e31fe867ff4f0775b1f688af
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2025-03-16 16:49:04 +0530

    feat(BL): Add AutoMapper profiles for product mapping
    - Introduced AutoMapper profiles for:
      - `ProductAddRequest` to `Product`
      - `ProductUpdateRequest` to `Product`
      - `Product` to `ProductResponse`
    
    - Updated `DependencyInjection.cs` to register
      AutoMapper service for dependency injection.
    
    - Enhances data representation conversion,
      promoting cleaner and maintainable code.

--- BusinessLogicLayer/DependencyInjection.cs
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BusinessLogicLayer.Mappers;
+using Microsoft.Extensions.DependencyInjection;
 
 
 namespace BusinessLogicLayer
@@ -7,7 +8,7 @@ namespace BusinessLogicLayer
     {
         public static IServiceCollection AddDataBusinessLogicLayer(this IServiceCollection services)
         {
-
+            services.AddAutoMapper(typeof(ProductAddRequestToProductMappingProfile).Assembly);
             return services;
         }
     }
