a0875108bb6df0f6a7245dbcab7c83b8aa946ab1
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2025-03-17 02:26:37 +0530

    feat(DI): Add service registrations and using directives
    - Added using directives for mappers, service contracts,
      services, and validators from BusinessLogicLayer and
      FluentValidation.
    
    - Introduced service registrations in
      `AddDataBusinessLogicLayer` method:
      - Added validators from the assembly containing
        `ProductAddRequestValidator`.
      - Registered `ProductsService` as the implementation
        for `IProductsService`.

--- BusinessLogicLayer/DependencyInjection.cs
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -1,4 +1,8 @@
 ﻿using BusinessLogicLayer.Mappers;
+using BusinessLogicLayer.ServiceContracts;
+using BusinessLogicLayer.Services;
+using BusinessLogicLayer.Validators;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 
@@ -9,6 +13,8 @@ namespace BusinessLogicLayer
         public static IServiceCollection AddDataBusinessLogicLayer(this IServiceCollection services)
         {
             services.AddAutoMapper(typeof(ProductAddRequestToProductMappingProfile).Assembly);
+            services.AddValidatorsFromAssemblyContaining<ProductAddRequestValidator>();
+            services.AddScoped<IProductsService, ProductsService>();
             return services;
         }
     }
