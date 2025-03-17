5191afde68734f44ff10553da9a792aa1afc3213
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2025-03-17 13:32:43 +0530

    feat(db): Update database configuration and schema
    - Modified `DependencyInjection.cs` to use environment
      variables for MySQL connection string.
    - Updated `db.sql` to create `ecommerceproductsdatabase`
      and `Products` table with 12 sample entries.
    - Adjusted `Dockerfile` to set `MYSQL_HOST` and
      `MYSQL_PASSWORD` environment variables.
    - Changed `appsettings.json` to utilize environment
      variables in the connection string for enhanced
      security and flexibility.

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -13,7 +13,12 @@ namespace DataAccessLayer
     {
         public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseMySQL(configuration.GetConnectionString("DefaultConnection")!));
+            string connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
+
+            string connectionString= connectionStringTemplate.Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST"))
+            .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD"));
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseMySQL(connectionString));
 
             services.AddScoped<IProductsRepository, ProductsRepository>();
             return services;
